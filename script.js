e8498c603790130934bfd94c7907d8ddcf1d27f4
Jhoan ramirez <stivenri99@gmail.com>
2024-11-10 10:55:31 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Formulario enviado");
+    // Aquí puedes agregar lógica para conectarte a la base de datos
+});
